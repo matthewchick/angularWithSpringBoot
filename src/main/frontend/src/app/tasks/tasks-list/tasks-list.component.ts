ef67932897781ef4e1a9e5ed1d9f364caf62b48d
Matthew Chick <c.chick@unsw.edu.au>
2019-11-14 20:51:11 +1100

    modify tasks list component

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from "src/app/tasks/task.model";
 
 @Component({
   selector: 'app-tasks-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
+  tasks: Task[] = [];   //tasks is an array object of Task
 
   constructor() { }
 
   ngOnInit() {
+    this.tasks.push( new Task(1, "Task 1", true, "12/11/19"));
+    this.tasks.push( new Task(2, "Task 2", false, "12/11/19"));
+    this.tasks.push( new Task(3, "Task 3", false, "12/11/19"));
   }
 
+  getDueDateLabel(task: Task){
+    return task.completed ? 'badge-success' : 'badge-primary';
+  }
+
+  onTaskChange(event, task) {
+    console.log('Task has been changed');
+  }
 }
